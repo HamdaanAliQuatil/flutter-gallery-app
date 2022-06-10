071b30e09b7a9dcce57959c1001f006fe5a64a4a
HamdaanAliQuatil <q.hmd08@gmail.com>
2022-06-10 16:03:29 +0530

    refactor: improve ui of gallery screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,27 @@ void main(){
   runApp(App());
 }
 
+class GalleryPage extends StatelessWidget {
+  String title;
+  List<String> urls;
+
+  GalleryPage({Key? key, required this.title, required this.urls}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.title),
+      ),
+      body: GridView.count(
+        primary: false,
+        crossAxisCount: 2,
+        children: List.of(urls.map((url) => Photo(url: url))),
+      ),
+    );
+  }
+}
+
 const List<String> urls =[
   "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
   "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
@@ -19,15 +40,24 @@ class App extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
       title: 'Photo Viewer',
-      home: Photo(),
-    );
+      home: GalleryPage(
+        title: 'Image Gallery',
+        urls: urls,
+      ),
+      );
   }
 }
 
 class Photo extends StatelessWidget{
-  final url = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
+  final url;
+
+  Photo({this.url});
+
   @override
   Widget build(BuildContext context){
-    return Image.network(url);
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Image.network(url),
+    );
   }
 }
