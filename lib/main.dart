8667ebac30b4312e44dab5a26580594234e686a8
HamdaanAliQuatil <q.hmd08@gmail.com>
2022-06-17 21:51:57 +0530

    feat: use streams and sink to manage state async

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
-// ignore_for_file: prefer_const_constructors_in_immutables, unnecessary_this, use_key_in_widget_constructors
+// ignore_for_file: prefer_const_constructors_in_immutables, unnecessary_this, use_key_in_widget_constructors, import_of_legacy_library_into_null_safe, prefer_final_fields, unused_field
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'dart:async';
 
 void main() {
-  runApp(App());
+  AppModel model = AppModel();
+  runApp(App(model));
 }
 
 const List<String> urls =[
@@ -14,96 +16,91 @@ const List<String> urls =[
   "https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2020/07/solar_orbiter_s_first_views_of_the_sun5/22136942-2-eng-GB/Solar_Orbiter_s_first_views_of_the_Sun_pillars.gif",
 ];
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
+  final AppModel model;
+
+  App(this.model);
+
   @override
-  AppModel createState() => AppModel();
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+            title: 'Photo Viewer',
+            home: GalleryPage(
+                      title: "Image Gallery", model: model
+                    ));
+  }
 }
 
 class PhotoState {
   String url;
-  late bool selected;
-  late bool display;
+  late bool selected = true;
+  late bool display = true;
   Set<String> tags = {};
 
   PhotoState(this.url, {selected = false, display = true, tags});
 }
 
-class MyInheritedWidget extends InheritedWidget {
-  final AppModel model;
+class AppModel with ChangeNotifier {
+  Stream<bool> get isTagging => _taggingController.stream;
+  Stream<List<PhotoState>> get photoStates => _photoStateController.stream;
 
-  MyInheritedWidget({Key? key, required Widget child, required this.model})
-      : super(key: key, child: child);
+  bool _isTagging = false;
+  List<PhotoState> _photoStates = List.of(urls.map((url) => PhotoState(url)));
 
-  static AppModel of(BuildContext context) {
-    return context
-        .dependOnInheritedWidgetOfExactType<MyInheritedWidget>()!
-        .model;
+  Set<String> tags = {"all", "nature", "logo", "book"};
+
+  // static AppModel of(BuildContext context) => ScopedModel.of<AppModel>(context);
+  StreamController<bool> _taggingController = StreamController.broadcast();
+  StreamController<List<PhotoState>> _photoStateController = StreamController.broadcast();
+
+  AppModel(){
+    _photoStateController.onListen = () {
+      _photoStateController.add(_photoStates);
+    };
+    _taggingController.onListen = () {
+      _taggingController.add(_isTagging);
+    };
   }
 
-  @override
-  bool updateShouldNotify(_) => true;
-}
-
-class AppModel extends State<App> {
-  bool isTagging = false;
-
-  List<PhotoState> photoStates = List.of(urls.map((url) => PhotoState(url)));
-
-  Set<String> tags = {"all", "nature", "cat"};
-
   void toggleTagging(String url) {
-    setState(() {
-      isTagging = !isTagging;
-      photoStates.forEach((element) {
-        if (isTagging && element.url == url) {
-          element.selected = true;
-        } else {
-          element.selected = false;
-        }
-      });
+    _isTagging = !_isTagging;
+    _photoStates.forEach((element) {
+      if (_isTagging && element.url == url) {
+        element.selected = true;
+      } else {
+        element.selected = false;
+      }
     });
+    _taggingController.add(_isTagging);
+    _photoStateController.add(_photoStates);
   }
 
   void onPhotoSelect(String url, bool selected) {
-    setState(() {
-      photoStates.forEach((element) {
-        if (element.url == url) {
-          element.selected = selected;
-        }
-      });
+    _photoStates.forEach((element) {
+      if (element.url == url) {
+        element.selected = selected;
+      }
     });
+    _photoStateController.add(_photoStates);
   }
 
   void selectTag(String tag) {
-    setState(() {
-      if (isTagging) {
-        if (tag != "all") {
-          photoStates.forEach((element) {
-            if (element.selected) {
-              element.tags.add(tag);
-            }
-          });
-        }
-        toggleTagging('');
-      } else {
-        photoStates.forEach((element) {
-          element.display = tag == "all" ? true : element.tags.contains(tag);
+    if (_isTagging) {
+      if (tag != "all") {
+        _photoStates.forEach((element) {
+          if (element.selected) {
+            element.tags.add(tag);
+          }
         });
       }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Photo Viewer',
-        home: MyInheritedWidget(
-            child: Builder(builder: (BuildContext innerContext) {
-              return GalleryPage(
-                  title: "Image Gallery",
-                  model: MyInheritedWidget.of(innerContext));
-            }),
-            model: this));
+      toggleTagging('');
+    } else {
+      _photoStates.forEach((element) {
+        element.display = tag == "all" ? true : element.tags.contains(tag);
+      });
+      _photoStateController.add(_photoStates);
+    }
+    notifyListeners();
   }
 }
 
@@ -117,15 +114,18 @@ class GalleryPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(this.title)),
-      body: Builder(builder: (BuildContext innerContext) {
-        return GridView.count(
-            primary: false,
-            crossAxisCount: 2,
-            children: List.of(model.photoStates
-                .where((ps) => ps.display)
-                .map((ps) => Photo(
-                    state: ps, model: MyInheritedWidget.of(innerContext)))));
-      }),
+      body: StreamBuilder<List<PhotoState>>(
+        initialData: const [],
+        stream: model.photoStates,
+        builder: (context, snapshot) {
+          return GridView.count(
+              primary: false,
+              crossAxisCount: 2,
+              children: List.of(snapshot.data!
+                  .where((ps) => ps.display)
+                  .map((ps) => Photo(state: ps, model:model))));
+        }
+      ),
       drawer: Drawer(
           child: ListView(
         children: List.of(model.tags.map((t) => ListTile(
@@ -148,13 +148,17 @@ class Photo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> children = [
+    return StreamBuilder<bool>(
+      initialData: false,
+      stream: model.isTagging,
+      builder: (context, snapshot){
+            List<Widget> children = [
       GestureDetector(
           child: Image.network(state.url),
           onLongPress: () => model.toggleTagging(state.url))
     ];
 
-    if (model.isTagging) {
+    if (snapshot.data != null) {
       children.add(Positioned(
           left: 20,
           top: 0,
@@ -174,5 +178,7 @@ class Photo extends StatelessWidget {
     return Container(
         padding: EdgeInsets.only(top: 10),
         child: Stack(alignment: Alignment.center, children: children));
+      }
+    );
   }
 }
